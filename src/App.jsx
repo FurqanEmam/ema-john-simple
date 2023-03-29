79e968960e98a79edb79f009ccae75aa104fa63e
FurqanEmam <furqanemam@gmail.com>
2023-03-29 15:19:52 +0600

    shop.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
